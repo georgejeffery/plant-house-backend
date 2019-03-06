ab4b8af6d3a7015dc10640717901f2ea46e60082
georgejeffery <georgejeffery@gmail.com>
2019-03-06 11:37:54 +0000

    More change!

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::AuthenticationController < API::V1::BaseController
   require 'jwt'
 
   def create
-    user = User.find_by(email: params[:email])
+    user = User.find_by(username: params[:username])
       if user && user.authenticate(params[:password])
         auth_token = JWT.encode({user_id: user.id}, secret)
         render json: {auth_token: auth_token}, status: :ok
